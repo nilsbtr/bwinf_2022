425b5121e67be3046c2ec6235912bdb871ee5b27
PunchedYou <nibot04@gmail.com>
2022-09-08 21:46:00 +0200

    deleted a import

--- src/junioren/_01reimerei/Aufgabe.java
+++ src/junioren/_01reimerei/Aufgabe.java
@@ -4,9 +4,8 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.lang.module.FindException;
-import java.util.regex.Pattern;
 import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Aufgabe {
     String dateiName;
